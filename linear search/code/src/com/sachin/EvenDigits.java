9db075b62216fd25a552e8b35ac833700792b4af
deepanshujoshi36 <deepanshujsndelhi@gmail.com>
2025-08-03 12:29:57 +0530

    completion of linear search

---
+++ linear search/code/src/com/sachin/EvenDigits.java
@@ -0,0 +1,30 @@
+//solution of problem no: 1295
+package com.sachin;
+
+public class EvenDigits {
+    public static void main(String[] args){
+        int[] arr = { -1231 , 456 , 12345 , 142 , 1 , 2 };
+        System.out.println(findNumbers(arr));
+    }
+
+    static int findNumbers(int[] arr){
+        int evenNum = 0 ;
+        for ( int i = 0 ; i < arr.length ; i++){
+            int temp = arr[i];
+
+            // counts the no of digits in a number
+            int count = 0;
+
+            //gives the count of a digit
+            while(temp != 0){
+                temp = temp / 10;
+                count ++;
+            }
+            if(count % 2 == 0){
+                evenNum ++;
+            }
+        }
+        return evenNum;
+    }
+}
+
---
+++ linear search/code/src/com/sachin/MaxWealth.java
@@ -0,0 +1,29 @@
+//Solution for problem no: 1672
+package com.sachin;
+
+public class MaxWealth {
+    public static void main(String[] args){
+        int[][] arr = {
+                {1 , 2  ,5 },
+                {3 , 5 , 6 },
+                {2 , 3 , 5 , 10}
+        };
+        System.out.println(maximumWealth(arr));
+    }
+    static int maximumWealth(int[][] accounts){
+        int maxWealth = 0;
+
+        //customer = row
+        //bank = column
+        for ( int customer = 0 ; customer < accounts.length ; customer++){
+            int sum = 0;
+            for( int bank = 0 ; bank < accounts[customer].length ; bank++){
+                sum = sum + accounts[customer][bank];
+            }
+            if(sum > maxWealth){
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
